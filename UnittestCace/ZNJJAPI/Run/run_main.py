1bd16c7d83d1cbe571ece0c67ec844765ed74d0d
yuanjunling <261412489@qq.com>
2020-03-27 15:14:03 +0800

    run_main

--- UnittestCace/ZNJJAPI/Run/run_main.py
+++ UnittestCace/ZNJJAPI/Run/run_main.py
@@ -5,12 +5,12 @@ from UnittestCace.ZNJJAPI.Util.handle_excle import handle
 
 class RunMain:
     def run_case(self):
+        #获取全部数据
         url = "E:/UnittestCaseDemo/UnittestCace/ZNJJAPI/Util/Excle_case/case_01.xlsx"
         rows = handle.get_rows(url)
         for i in range(rows):
             print("----->",i)
             data = handle.get_rows_value(url,i+2)
-            print(data)
             print(data)
 
 if __name__=="__main__":
