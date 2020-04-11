60af78815e00e1f3a45180b22e9c51df994dd679
261412489@qq.conm <admin@890903>
2020-04-12 00:17:33 +0800

    错误提示优化

--- Study/if_case.py
+++ Study/if_case.py
@@ -1,9 +1,28 @@
 import random
 Num1 = random.randint(0,10)
 Num2 = random.randint(0,10)
-if Num1 > Num2:
-     print('1赢了')
-elif Num1 == Num2:
-     print('平局')
-else:
-     print('2赢了')
+
+class name:
+     def name1(self):
+          pass
+     def name2(self):
+          pass
+     def name3(self):
+          pass
+if __name__ == '__main__':
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
