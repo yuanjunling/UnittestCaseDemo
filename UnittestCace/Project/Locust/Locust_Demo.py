22fe60f600414b8a0d7017b92b34d8d26f2c2799
261412489@qq.conm <admin@890903>
2020-06-16 22:10:52 +0800

    自定义代码格式

--- UnittestCace/Project/Locust/Locust_Demo.py
+++ UnittestCace/Project/Locust/Locust_Demo.py
@@ -1,10 +1,11 @@
 from locust import HttpUser, task, between, SequentialTaskSet, tag
-
+import random
 class MyTaskCase(SequentialTaskSet):
     def on_start(self):
         pass
 
     @task
+    @tag("leave_1")
     def regist_(self):  # 一个方法， 方法名称可以自己改
         url = '/user/login'  # 接口请求的URL地址
         self.headers = {"Content-Type": "application/json"}  # 定义请求头为类变量，这样其他任务也可以调用该变量
@@ -12,10 +13,24 @@ class MyTaskCase(SequentialTaskSet):
         # 使用self.client发起请求，请求的方法根据接口实际选,
         # catch_response 值为True 允许为失败 ， name 设置任务标签名称   -----可选参数
         with self.client.post(url, json=data, headers=self.headers, catch_response=True) as rsp:
-            if rsp.status_code == 200:
+            if rsp.status_code > 400:
                 print(rsp.text)
-                rsp.failure('regist_ 接口成功')
+                rsp.failure('regist_ 接口失败')
 
+    @task  # 装饰器，说明下面是一个任务
+    def login_(self):
+        url = '/user/register'  # 接口请求的URL地址
+        userAccount = random.randint(10000, 100000)
+        data = {"userAccount":userAccount,"userPassword":"e10adc3949ba59abbe56e057f20f883e"}  # post请求的 请求体
+        with self.client.post(url, json=data, headers=self.headers, catch_response=True) as rsp:
+            self.code = rsp.json()['code']  # 提取响应json 中的信息，定义为 类变量
+            if  self.code == 80002:
+                rsp.success()
+            else:
+                rsp.failure('login_ 接口失败！')
+
+    def on_stop(self):
+        pass
 class UserRun(HttpUser):
     tasks = [MyTaskCase]
     wait_time = between(0.1, 3)
